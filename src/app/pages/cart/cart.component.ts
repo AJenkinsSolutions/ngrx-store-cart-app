3ef2e765093b182878e1908934b040aa9f7c845e
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-10 09:20:20 -0500

    Add: Init CartItems$ in CartComponent, Display items in html

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { AppState } from '../../NGRX-states/app.state';
 import { Store } from '@ngrx/store';
+import { seletctCartProducts } from '../../NGRX-selectors/cart.selector';
+import { Observable } from 'rxjs';
+import { IProduct } from '../../models/product.interface';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
+
+  //Now we can use this data in the cart HTML
+  cartItems$: Observable<IProduct[]> = this.store.select(seletctCartProducts)
 
   constructor(private store: Store<AppState>){}
 
