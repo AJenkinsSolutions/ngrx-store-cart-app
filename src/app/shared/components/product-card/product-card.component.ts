c4317c6dae1edbc4679ae393494634fbaa452fc2
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-10 08:31:22 -0500

    Add:  Action dispatcher in the child Component

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, Output,EventEmitter } from '@angular/core';
 import { IProduct } from '../../../models/product.interface';
+
 
 @Component({
   selector: 'app-product-card',
@@ -14,11 +15,30 @@ import { IProduct } from '../../../models/product.interface';
 })
 export class ProductCardComponent { 
 
+  // !! Dumb Component !!! should only be responsible for displaying
+
   //This component is now the 'child component' of the ProductComponent
   //Meaning it will receive data from the parent component
 
 
+  // The Input means it is taking 'IN' data from the parent component
   @Input() product!: IProduct;
+
+  //Output means we are 'Outputting' something from the UI *In this case its a product obj*
+  //Dispatch Events triggered in the UI 
+  @Output() handleAdd = new EventEmitter();
   constructor(){}
+
+
+
+
+  // Event emitting in child component
+  addToCart(product : IProduct){
+
+    // Please handle add
+    // please Emitt the value, (product)
+    this.handleAdd.emit(product)
+
+  };
   
 }
