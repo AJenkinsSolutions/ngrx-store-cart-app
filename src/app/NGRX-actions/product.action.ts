22877f44b095776a7ae197c6b4adaef3b4c671e3
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-03-11 14:52:38 -0500

    Add: Product Actions

---
+++ src/app/NGRX-actions/product.action.ts
@@ -0,0 +1,15 @@
+import { createAction, props } from "@ngrx/store";
+
+import { IProduct } from "../models/product.interface";
+
+
+export const loadProduct = createAction(
+    '[Prodcut Component] LoadProduct');
+
+export const loadProductSuccess = createAction(
+    '[Product Component] LoadProductSuccess',
+     props<{products: IProduct[]}>)
+    
+export const loadProductFailure = createAction(
+    '[Product Component] LoadProductFailure'
+    , props<{errorMessage: string}>);
